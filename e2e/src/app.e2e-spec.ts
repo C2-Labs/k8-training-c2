b63f992f4692040d9e68a134e3db1b69959de692
Jared Howerton <howieaic@gmail.com>
2020-04-06 12:21:30 -0400

    generalized

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('entergy-demo app is running!');
+    expect(page.getTitleText()).toEqual('k8s-training app is running!');
   });
 
   afterEach(async () => {
